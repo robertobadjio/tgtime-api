7d4adef5d03d9a51de30a69ac944d185a261924e
Robert <fatkhullin.robert@gmail.com>
2020-09-16 18:50:26 +0300

    Fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,9 +6,9 @@
 . deploy/deploy.config
 
 #go build -o ../build/officetime cmd/officetime/main.go
-env GOOS=linux GOARCH=amd64 go build -o ../build/o_api -v main.go
+env GOOS=linux GOARCH=amd64 go build -o build/o_api -v main.go
 ssh root@$removeServer 'systemctl stop officetime-api.service && rm /var/officetime/o_api'
-scp ../build/o_api root@$removeServer:/var/officetime/
+scp build/o_api root@$removeServer:/var/officetime/
 ssh root@$removeServer 'systemctl start officetime-api.service'
 
 exit 0
